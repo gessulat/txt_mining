cff21fb0a3eae5c0e22a8af4968a246b4c9700be
schwarsi <simonschwarzmeier@gmail.com>
2012-12-09 20:40:18 +0100

    using nltk-tokenizer instead of splitting by ' '

--- abstracts2dict.py
+++ abstracts2dict.py
@@ -1,14 +1,23 @@
 import pickle
 from stemming.porter2 import stem
-
+from fish import ProgressFish
+from nltk.tokenize import wordpunct_tokenize
 def main():
-	out = file('../words.pickle', 'w')
-	abstracts = pickle.load('abstracts.pickle')
+	out = open('../words.pickle', 'w')
+	abstracts = pickle.load(open('../dense_abstracts.pickle'))
 	words = []
+
+	fish = ProgressFish(total=len(abstracts))
+	cnt = 0
 	print "reading all words"
 	for (key, abstract) in abstracts.items():
-		for word in abstract:
-			words.append( stem(word) )
+		sentence = wordpunct_tokenize(abstract.lower())
+		for word in sentence:
+			if word.isalnum():
+				words.append( stem(word) )
+		cnt += 1
+		fish.animate(amount=cnt)
+
 	print "removing duplicates"
 	words = set(words)
 	pickle.dump(words, out)
