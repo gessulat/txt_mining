fbbb27e38fb0ae7cc7880265a4786ae51689a453
schwarsi <simonschwarzmeier@gmail.com>
2012-12-09 18:15:15 +0100

    fix (second)

--- synchronize_by_diff.py
+++ synchronize_by_diff.py
@@ -1,5 +1,6 @@
 import pickle
 import argparse
+from datetime import datetime
 
 def main():
 	parser = argparse.ArgumentParser()
@@ -16,15 +17,20 @@ def main():
 	diffFilePath = args.diff
 	diff_file = open(diffFilePath)
 	diff = pickle.load(diff_file)
-		
+	
+	warn = False
+
 	print str(datetime.now())+ 'appending diff ', diff.name, ' to ', old.name
 
 	for article_id, _ in diff.items():
 		if old.has_key(article_id):
 			del old[article_id]
 		else:
-			print 'Error'
-			return -1
+			print 'WARNING: the two documents dont match' 
+			warn = True
+
+	if warn:
+		print 'WARNING: The documents do not have exactly the same base'
 
 	outFilePath = args.out
 	out_file = open(outFilePath, 'w')
