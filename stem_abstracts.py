8dc81d419157a176587553c28cf56ec0c840345d
schwarsi <simonschwarzmeier@gmail.com>
2013-01-07 16:23:45 +0100

    renamed old abstracts2dict.py to stem abstracts; takes arguments from command line from now on

--- stem_abstracts.py
+++ stem_abstracts.py
@@ -1,18 +1,28 @@
-import cPickle
+import cPickle, argparse
 from stemming.porter2 import stem
 from fish import ProgressFish
 from nltk.tokenize import wordpunct_tokenize
 
 def main():
-	dictionary = open('../words.pickle', 'w')
-	stemmed_abstracts_file = open('../stemmed_abstracts.pickle', 'w')
-	abstracts = cPickle.load(open('../english_abstracts.pickle'))
+	parser = argparse.ArgumentParser()
+	parser.add_argument('in_abs', help='input abstracts file path: "../*_abstracts.pickle" ')
+	parser.add_argument('out_stemmed_abs', default='../stemmed_abstracts.pickle', help='file path of abstracts output file: "stemmed_abstracts.pickle"')
+	parser.add_argument('out_words', default='../word_base.pickle', help='file path of words output file: "word_base.pickle"')
+
+	args = parser.parse_args()
+
+	word_base = open(out_words, 'w')
+	stemmed_abstracts_file = open(out_stemmed_abs, 'w')
+	abstracts = cPickle.load(open(args.in_abs))
+
+	print "loading pickles..."
+	
 	words = []
 	stemmed_abstracts = {}
 
 	fish = ProgressFish(total=len(abstracts))
 	cnt = 0
-	print "reading all words"
+	print "reading all words..."
 	for (key, abstract) in abstracts.items():
 		sentence = wordpunct_tokenize(abstract.lower())
 		new_sentence = []
@@ -27,7 +37,7 @@ def main():
 
 	print "removing duplicates"
 	words = set(words)
-	cPickle.dump(words, dictionary)
+	cPickle.dump(words, word_base)
 
 	print "persisting data"
 	
