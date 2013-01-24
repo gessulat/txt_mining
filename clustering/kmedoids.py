4c6dd3c616fc675b60dabb107985455102fb1831
Sigi <s.gessulat@gmail.com>
2013-01-24 15:06:55 +0100

    new kmedoids.py

--- clustering/kmedoids.py
+++ clustering/kmedoids.py
@@ -1,11 +1,44 @@
-from nltk.cluster.util import cosine_distance
-import scipy
-from numpy import array
+from Pycluster import kmedoids
+from numpy import array, loadtxt 
+from scipy.spatial import distance
+import pickle
 
-def tanimoto(a, b):
-	X = array(a,b)
-	scipy.spatial.distance.pdist(X,)
+def initalizeClusters( hcores, distances ):	
+	print len(distances)
+	distances = distance.squareform(distances, checks=True)
 
-def generic_distance( a, b, distFunc ):
-	return distFunc(a,b)
+	init = array()	
+	return None
 
+print 'loading distance matrix...'
+f = open('data/mr_10k_fishDist_50.p')
+distances = pickle.load(f)
+f.close()
+
+
+
+
+print 'loading hcores...'
+cores = loadtxt('data/cores/cores_mr_10k_l50_r50.p')
+
+distances = distance.squareform(distances, checks=True)
+print len(distances)
+for dist in distances:
+	minimum = None
+	print len(dist)
+
+
+
+print 'cores: '+ str(len(cores))
+print cores
+# unneccesary
+# print 'unfold compressed matrix...'
+# dist = distance.squareform(dist, checks=True)
+
+init = None	
+n = 12
+passes = 10
+
+'clustering with kmedoids'
+result = kmedoids(distance = dist, npass = passes, nclusters = n, initialid = init)
+print result
