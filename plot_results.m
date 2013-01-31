9a3dea6efda87f73e2f0d8ae261a3dfb562a4b3b
schwarsi <simonschwarzmeier@gmail.com>
2013-01-31 10:46:00 +0100

    added plot against no_clusters

--- plot_results.m
+++ plot_results.m
@@ -18,6 +18,7 @@ for k = [1:size(Res,2)]
 		labels = [labels;strcat('lambda-',num2str(lambda))];
 		idx = lambdas == lambda;
 		plot(R(idx,3), R(idx,5), '@-','color', cc(color_idx, :));
+		text(R(idx,3), R(idx,5), num2str(R(idx,4)), 'horizontalalignment','left', 'verticalalignment', 'top');
 		hold on;
 		color_idx = color_idx +1;
 	end
@@ -25,5 +26,30 @@ for k = [1:size(Res,2)]
 	title(strcat('K-Mediods- Error for ', sampling_method));
 	xlabel('r-values');
 	ylabel('error');
+	legend(cellstr(labels));
+end
+
+for k = [1:size(Res,2)]
+	R = Res{k};
+	sampling_method = sampling_methods{k};
+
+	lambdas = R(:,2);
+	lambda_range = intersect(lambdas, min(lambdas):max(lambdas));
+
+	labels = [];
+	cc=hsv(length(lambda_range)+5);
+	color_idx = 1;
+	figure;
+	for lambda = lambda_range
+		labels = [labels;strcat('lambda-',num2str(lambda))];
+		idx = lambdas == lambda;
+		plot(R(idx,3), R(idx,4), '@-','color', cc(color_idx, :));
+		hold on;
+		color_idx = color_idx +1;
+	end
+
+	title(strcat('K-Mediods- No of Clusters _', sampling_method));
+	xlabel('r-values');
+	ylabel('#Clusters');
 	legend(cellstr(labels));
 end
